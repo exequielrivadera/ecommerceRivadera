815319c518acc11af2bc2988638240f35c04163e
exequielrivadera <exequielrivadera@gmail.com>
2022-12-14 15:51:50 -0300

    Segunda Pre-Entrega

---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const data = [
+	{
+		id: 1,
+		img: 'https://ferreira.vtexassets.com/arquivos/ids/364324-800-auto?v=637448166383400000&width=800&height=auto&aspect=true',
+		nameProduct: 'Zapatillas',
+		price: 25000,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1099&q=80',
+		nameProduct: 'Reloj',
+		price: 32500,
+		quantity: 1,
+	
+	},
+];
